9eff1738d5647e78fcc33070e1f7d284ec507917
NamSupawan <60772519+NamSupawan@users.noreply.github.com>
2022-10-20 17:51:47 +0700

    สเตตัส

--- src/LazadaClient/types/Order.js
+++ src/LazadaClient/types/Order.js
@@ -1,13 +1,16 @@
 /**
  * @flow
  */
-export type OrderStatus =
-  | 'unpaid'
-  | 'pending'
-  | 'packed'
-  | 'canceled'
-  | 'ready_to_ship'
-  | 'delivered'
-  | 'returned'
-  | 'shipped'
-  | 'failed'
+ export type OrderStatus =
+ | 'unpaid'
+ | 'pending'
+ | 'packed'
+ | 'canceled'
+ | 'ready_to_ship'
+ | 'delivered'
+ | 'lost_by_3pl' 
+ | 'damaged_by_3pl' 
+ | 'failed_delivery' 
+ | 'returned'
+ | 'shipped'
+ | 'failed'
